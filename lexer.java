8e40de170981c43cf894907f81b64ab96c345ef9
catoja1107 <jcato195@gmail.com>
2022-07-24 22:34:16 -0400

    Create lexer.java

---
+++ lexer.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class recursiveDescent {
+  private static class Parser {
+    private StringBuilder buffer;
+    public Parser(File file) {
+      Scanner sc = null;
+      try {
+        sc = new Scanner(file);
+        buffer = new StringBuilder((int) file.length());
+        while (sc.hasNext()) {
+          String token = sc.next();
+          StringTokenizer tokens = new StringTokenizer(token, '(',')','+','-','*','/','=',';',"switch","foreach","for","while","do-while","block","if","assignment","return", true);
+
+          while (tokens.hasMoreTokens())
+            buffer.append(tokens.nextToken() + " ");
+        }
+      } catch (FileNotFoundException e) {
+        System.out.print("File not found.");
+        e.printStackTrace();
+      } finally {
+        sc.close();
+      }
+    }
+    private String lexer() {
+      int i = buffer.indexOf(" ");
+      String lexeme = buffer.substring(0, i);
+      buffer.delete(0, i + 1);
+      System.out.println(lexeme); //debug
+      return lexeme;
+    }
+    public boolean analyzeCode() {
+      return program();
+    }
+    private boolean variable() {
+      if (!lexer().matches("[a-zA-Z0-9]*")) return false;
+      return true;
+    }
+    private boolean program() {
+      if (!lexer().toLowerCase().equals("start")) return false;
+      if (!lexer().toLowerCase().equals("end")) return false;
+      if (!variable()) return false;
+      return true;
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("File name: ");
+    String name = sc.next();
+    File file = new File(name);
+    Parser parser = new Parser(file);
+    if (!parser.analyzeCode())
+      System.out.print("There are no syntax errors in the program.");
+
+    else
+      System.out.print("The program contains syntax errors.");
+  }
+}
